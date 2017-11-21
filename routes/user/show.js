448728a7ee795165293971e8ecab3e33818b930c
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-11-21 20:19:28 +0100

    Home + Profiles

--- routes/user/show.js
+++ routes/user/show.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 const User = require('../models/user');
+
+router.get('/', function(req, res, next) {
+  User.find()
+    .then(result => {
+      console.log(result)
+      res.render('/', {result})
+    })
+});
+
+module.exports = router;
